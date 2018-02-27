b2e31beefac48b6383d5caaf01b94c09a21c542f
T1\Thet.Wai <thet.wai@traceone.com>
2018-02-27 15:28:25 +0000

    Add advance features like deleting, spliting components, etc.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
+import TodoList from './TodoList'
 
 export default class App extends Component {
   constructor(props) {
@@ -18,12 +19,25 @@ export default class App extends Component {
   }
 
   handleAddTask = () => {    
-    
-    this.setState((prevState) => {
-        prevState.tasks.push(this.state.value);       
+    if (this.state.value) {
+      this.setState((prevState) => {
+          prevState.tasks.push(this.state.value);       
+          return {
+            tasks: prevState.tasks,
+            value: ''
+          };
+        });
+    }
+    else {
+      alert('please type something to add');
+    }
+  }
+
+  handleDelete = (i) => {
+    this.setState((prevState) => {        
+        prevState.tasks.splice(i, 1);
         return {
-          tasks: prevState.tasks,
-          value: ''
+          tasks: prevState.tasks         
         };
       });
   }
@@ -34,7 +48,7 @@ export default class App extends Component {
         <div className="App">        
           <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Basic Todo</h1>
+          <h1 className="App-title">Todo</h1>
         </header>
         </div>
         <hr />  
@@ -48,14 +62,17 @@ export default class App extends Component {
               }}
           />&nbsp;
           <input type="button" value="Add Task" id="btnAddTask" onClick={this.handleAddTask} />
-        </div>   
-        <div>
-          <ol>
-            {this.state.tasks.map((item, i) =>            
-                <li key={i}>{item}</li>
-            )}
-          </ol>
-        </div>
+        </div>           
+        <TodoList 
+          header="Todo List 1" 
+          tasks={this.state.tasks} 
+          handleDelete={this.handleDelete}
+          />
+        <TodoList 
+          header="Todo List 2" 
+          tasks={this.state.tasks} 
+          handleDelete={this.handleDelete} 
+          />
       </div>
     );
   }
---
+++ src/components/TodoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './TodoList.css';
+
+const TodoList = props => {    
+
+    return (
+        <div className="todoBorder">
+            <h2>{props.header}</h2>
+            {props.tasks.length ?
+                <ol>                     
+                {props.tasks.map((item, i) =>            
+                    <li key={i}>{item}&nbsp;&nbsp;&nbsp;<a onClick={() => props.handleDelete(i)} className="deleteButton" title="Remove">X</a></li>
+                )}
+                </ol>
+                : <h4>No items yet</h4>
+            }
+        </div>
+    );
+};
+
+export default TodoList;
